a554bf9caf494f90d81aa80ef973e69913b9fa43
姚彦勋 <991030857@qq.com>
2018-07-19 13:11:16 +0800

    inital project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Johnathon Yao
+      </div>
+    );
+  }
+}
+
+export default App;
